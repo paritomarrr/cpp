928458c3d83162a052e7447df2b757896959c7ca
Pari Tomar <68550407+paritomarrr@users.noreply.github.com>
2022-01-21 09:00:13 +0530

    prod

---
+++ prodofmaxandmin.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int prod(int arr, int arr2)
+{
+    int max = arr[0];
+    int min = arr2[0];
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if ()
+        }
+    }
+};
+int main()
+{
+    int arr, arr2;
+    cout << "arr1" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    };
+    cout << "arr2" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr2[i];
+    };
+}
