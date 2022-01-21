48beac710d88844e9a07ea3aaae2d43ec7d95f2e
Pari Tomar <68550407+paritomarrr@users.noreply.github.com>
2022-01-21 11:22:24 +0530

    time comp of o(n)

--- functions/fibo.cpp
+++ functions/fibo.cpp
@@ -2,19 +2,31 @@
 using namespace std;
 int fibo(int num)
 {
+    // int sum = 0;
+    // for (int i = 0; i < num; i++)
+    // {
+    //     if (i == 0)
+    //     {
+    //         sum = 0;
+    //         // cout << sum << endl;
+    //     }
+    //     else
+    //     {
+    //         sum = sum + i;
+    //         // cout << sum << endl;
+    //     }
+    // }
+    // return sum;
+    int t1 = 0;
+    int t2 = 0;
     int sum = 0;
-    for (int i = 0; i < num; i++)
+    for (int i = 1; i < num; i++)
     {
-        if (i == 0)
-        {
-            sum = 0;
-            // cout << sum << endl;
-        }
-        else
-        {
-            sum = sum + i;
-            // cout << sum << endl;
-        }
+        cout << t1 << endl;
+        t2++;
+
+        t1 = t1 + t2;
+        // t2++;
     }
     return sum;
 };
@@ -22,6 +34,7 @@ int main()
 {
     int fibno;
     cin >> fibno;
-    cout << fibo(fibno) << endl;
+    // cout << fibo(fibno) << endl;
+    fibo(fibno);
     return 0;
 }
