7d6a82eda0eb9128d4987363ced287bf503d4588
Pari Tomar <68550407+paritomarrr@users.noreply.github.com>
2022-01-31 11:28:58 +0530

    rowwisesum

---
+++ array/rowwisesum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[3][3];
+    cout << "array enter krdo bhai " << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << " Ye rhi array aur uska row-wise sum " << endl;
+    int sum = 0;
+    for (int i = 0; i < 3; i++)
+    {
+
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+            sum += arr[i][j];
+        }
+        cout << " sum -> " << sum << endl;
+        sum = 0;
+    }
+    return 0;
+}
